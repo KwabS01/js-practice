7a549b9d7485028469bd6f193f49610fe4e2650e
Kay <Kagyemang201@gmail.com>
2023-05-13 16:20:38 -0700

    Javascript Practice

---
+++ myscript.js
@@ -0,0 +1,45 @@
+//String Variables
+
+let name = "Cities";
+let job = "artist";
+let language = "Javascirpt";
+let footwear = "nike";
+let phone = "Samsung";
+
+
+//Integers Variables
+let age = 18
+let weight = 90
+let temperature = -18
+let house  = 12
+let height = 8
+
+
+//Float Variables
+let grade = 89.9
+let GPA = 3.0
+let amount = 800.9
+let article = 5.2
+let version = 2.4
+
+//Boolean Variables
+
+const players = true;
+const farmers = true;
+const artists = false;
+const games = true;
+const work = false;
+
+//Document Variables
+
+       document.write("players");
+	   document.write("version");
+	   document.write("farmers");
+	   document.write("work");
+	   document.write("games");
+	   document.write("article");
+	   document.write("amount");     
+	   document.write("temperature");
+	   document.write("age");
+	   document.write("phone");
+	   console.log("Getting Started with VScode");
